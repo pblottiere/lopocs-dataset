c304b4a7b616b49e60621450b13cbd1071021f50
Blottiere Paul <blottiere.paul@gmail.com>
2016-10-11 09:21:21 +0200

    update terrain dataset

--- terrain/cesium.sh
+++ terrain/cesium.sh
@@ -4,4 +4,5 @@ DB=lopocs_demos_terrain_cesium
 VENV=~/.virtualenvs/lightpcss
 
 dbbuilder -db $DB -table pa -host localhost -user blottiere \
-  -size 500 -files terrain.las -venv $VENV -target 3dtiles -srid 32616
+  -size 500 -files terrain.las -venv $VENV -target 3dtiles -srid 32616 \
+  -lodmax 4
--- terrain/potree.sh
+++ terrain/potree.sh
@@ -4,4 +4,4 @@ DB=lopocs_demos_terrain_potree
 VENV=~/.virtualenvs/lightpcss
 
 dbbuilder -db $DB -table pa -host localhost -user blottiere -size 500 \
-  -files terrain.las -venv $VENV -target greyhound -srid 32616
+  -files terrain.las -venv $VENV -target greyhound -srid 32616 -lodmax=6
